b8230c28e11d2bfba31152b7ceae9c49dc7a28c0
LuisFrancisco95 <uribe_morelos@hotmail.com>
2017-08-23 17:09:02 -0500

    Probando Boton Paa verificar El cine

--- app/src/main/java/com/example/luisfrancisco/sinfilarregional/Main2Activity.java
+++ app/src/main/java/com/example/luisfrancisco/sinfilarregional/Main2Activity.java
@@ -1,22 +1,49 @@
 package com.example.luisfrancisco.sinfilarregional;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner spProvincias;
-    Spinner spLocalidades;
+    private Spinner provincas;
+    private Spinner localidades;
+    Spinner spProvincias = provincas;
+    Spinner spLocalidades = localidades;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
+
+        Button btnBuscar  = (Button)findViewById(R.id.button2);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(spProvincias.equals("Coahuila")  && spLocalidades.equals("Saltillo"))
+                {
+                    startActivity(new Intent(Main2Activity.this, Cine_Saltillo.class));
+                }else if (spProvincias.equals("Coahuila") && spLocalidades.equals("Torreon"));
+                    startActivity(new Intent(Main2Activity.this, Main2Activity.class));
+
+            }
+        });
+
+
+
+
+
         
         this.spProvincias = (Spinner)findViewById(R.id.sp_provincia);
         this.spLocalidades = (Spinner)findViewById(R.id.sp_localidad);
